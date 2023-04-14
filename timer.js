b726ec4769bdc78ea9a26cb08789edf0ac4c203e
dehuk <denis.kalchuk@gmail.com>
2023-04-14 21:34:59 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("May 21, 2023 10:00:00").getTime();
+var countDownDate = new Date("May 21, 2023 09:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
